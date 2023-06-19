1c6a43ea49ea25626639bbef674033b209f10d15
hasibul1670 <hasibulislam1670@gmail.com>
2023-06-19 18:13:35 +0600

    duu

--- src/Pages/Shared/NavBar.jsx
+++ src/Pages/Shared/NavBar.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 const NavBar = () => {
   const [isDarkMode, setDarkMode] = React.useState(false);
@@ -7,21 +9,38 @@ const NavBar = () => {
     setDarkMode(checked);
   };
   useEffect(() => {
-    // This function will be called every time the state changes
+    const navbar = document.querySelector(".navbar");
+
     if (isDarkMode) {
+      navbar.style.backgroundColor = "black";
       document.body.style.backgroundColor = "black";
       document.body.style.color = "white";
     } else {
+      navbar.style.backgroundColor = "white";
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
     }
   }, [isDarkMode]);
 
+  const navOptions = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/menu">Our Course</Link>
+      </li>
+      <li>
+        <Link to="/order/salad">Events</Link>
+      </li>
+    </>
+  );
+
   return (
     <>
-      <div className="navbar bg-blue-200 fixed  z-10 bg-opacity-30 max-w-screen-xl font-bold ">
+      <div className="navbar fixed  z-10 bg-opacity-30 max-w-screen-xl font-bold ">
         <div className="navbar-start">
-          <div className="dropdown">
+          <div className=" dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -38,40 +57,18 @@ const NavBar = () => {
                 />
               </svg>
             </label>
+
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-compact bg-gray-400  dropdown-content mt-3 p-2 shadow rounded-box w-52"
             >
-              <li>
-                <a href="#">Menu Item 1</a>
-              </li>
-              <li>
-                <a href="#">Menu Item 2</a>
-              </li>
-              <li>
-                <a href="#">Menu Item 3</a>
-              </li>
-              {/* Add more menu items as needed */}
+              {navOptions}
             </ul>
           </div>
           <a className="btn btn-ghost normal-case text-xl">School</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a href="#">Menu Item 4</a>
-            </li>
-            <li>
-              <a href="#">Menu Item 5</a>
-            </li>
-            <li>
-              <a href="#">Menu Item 6</a>
-            </li>
-            <li>
-              <a href="#">Menu Item 6</a>
-            </li>
-            {/* Add more menu items as needed */}
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
         </div>
         <DarkModeSwitch
           checked={isDarkMode}
@@ -84,7 +81,14 @@ const NavBar = () => {
         />
 
         <div className="navbar-end">
-          <a className="btn">Get started</a>
+          <Link to="/dashboard/mycart">
+            <button className="btn btn-ghost mr-2">
+              <FaShoppingCart></FaShoppingCart>
+              <div className="badge badge-outline  badge-primary">+ 0</div>
+            </button>
+          </Link>
+
+          <button className="btn btn-outline btn-primary">Sign In</button>
         </div>
       </div>
     </>
