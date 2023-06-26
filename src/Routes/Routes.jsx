69e6f31e188671aba4574e17a5840593603695d4
hasibul1670 <hasibulislam1670@gmail.com>
2023-06-27 02:08:58 +0600

    sa

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
+import Login from "../Pages/Login/Login";
 import AboutUs from "../Pages/Shared/AboutUs";
 import NotFound from "../Pages/Shared/NotFound";
-import Home from "./../Pages/Home/Home";
-import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import Home from "./../Pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
@@ -16,13 +16,14 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: 'login',
-        element: <Login></Login>
+        path: "signup",
+        element: <SignUp></SignUp>,
       },
       {
-        path: 'signup',
-        element: <SignUp></SignUp>
+        path: "login",
+        element: <Login></Login>,
       },
+
       {
         path: "about-us",
         element: <AboutUs />,
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
+
+import AuthProvider from "./Providers/AuthProvider";
 import { router } from "./Routes/Routes";
 import "./index.css";
 
@@ -10,13 +12,14 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <div className="max-w-screen-xl  mx-auto">
-          <RouterProvider router={router} />
-        </div>
-      </QueryClientProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className="max-w-screen-2xl   mx-auto w-full">
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
